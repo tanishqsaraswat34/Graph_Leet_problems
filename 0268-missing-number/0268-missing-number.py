22c0c953968280f596e68bc18111c594d6de51fb
tanishqsaraswat34 <151999977+tanishqsaraswat34@users.noreply.github.com>
2026-08-21 08:52:25 +0530

    Time: 3 ms (60.58%), Space: 20.6 MB (18.47%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,12 +1,10 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         n=len(nums)
-        if n not in nums:
-            return n
-        i=0
-        while i<=n:
-            if i not in nums:
-                return i
-            i+=1
+        ans=n
+        for i in range(n):
+            ans^=i
+            ans^=nums[i]
+        return ans
     
         
